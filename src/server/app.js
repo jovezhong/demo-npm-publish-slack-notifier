df685abedd8ce0f653e79f42d785a6dfe7daef48
Jove Zhong <jovezhong@gmail.com>
2021-11-05 21:59:13 -0700

    add a simple web page to send numbers to kafka via buttons

--- src/server/app.js
+++ src/server/app.js
@@ -35,5 +35,31 @@ module.exports = ({ producer, config }) => {
     }
   );
 
+  //reuse this restify server for the web page and actions
+  function respondHome(req,res,next) {
+    res.sendRaw('<html><head><title>Demo Page</title></head><body><iframe style="display: none;" name=theFrame></iframe><p>Hey there. Please click a button:</p><div style="display:flex"><form action=send/10 method=post target=theFrame style="padding: 0 12px"><button>10 USD</button></form><form action=send/20 method=post target=theFrame style="padding: 0 12px"><button>20 USD</button></form><form action=send/30 method=post target=theFrame style="padding: 0 12px"><button>30 USD</button></form><form action=send/40 method=post target=theFrame style="padding: 0 12px"><button>40 USD</button></form><form action=send/50 method=post target=theFrame style="padding: 0 12px"><button>50 USD</button></form></div></body></html>');
+    next();
+  }
+  server.get('/', respondHome);
+  function respondSend(req,res,next) {
+    try {
+      producer.send({
+        topic: config.topic,
+        messages: [
+          {
+            key: 'key',
+            value: ''+req.params.number,
+          },
+        ],
+      });
+    } catch (error) {
+      console.error(`Failed to publish webhook message`, error);
+    }
+    res.sendRaw('got number '+req.params.number);
+    next();
+  }
+  server.post('/send/:number', respondSend);
+  
+
   return server;
 };
